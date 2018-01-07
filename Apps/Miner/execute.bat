7f71679fdff720420a215ad1d55b082b881c03ce
Mihail Maldzhanski <pollarize@gmail.com>
2018-01-07 12:51:13 +0200

    Update to use new commands.

--- Apps/Miner/execute.bat
+++ Apps/Miner/execute.bat
@@ -15,24 +15,24 @@ echo "9. Mining - Operator threshold - CUDA Accelerated"
 set /p choice="Selcet from [1..9]"
 echo.
 if "%choice%"=="1" (
-    node index.js -c miner.json ./out/unitTest.txt 3SEdktQGS4K947PUadvbHFD2oJG
+    node index.js -c ./examples/miner.json ./out/unitTest.txt 3SEdktQGS4K947PUadvbHFD2oJG
 ) else if "%choice%"=="2" (
     set /p value="Select DICE Value [1/1024..1024]"	
 	goto:diceVal
 ) else if "%choice%"=="3" (
-     node index.js -v ./out/unitTest.txt.1
+     node index.js -v ./out/unitTest.txt
 ) else if "%choice%"=="4" (
     node index.js -k keyPair.json
 ) else if "%choice%"=="5" (
-    node index.js -tc miner.json ./out/unitTest.txt ./out/unitEncToM2.txt 2dnzkaaKeeCeUAXTy2DrxijSKGB 3SEdktQGS4K947PUadvbHFD2oJG
+    node index.js -tc ./examples/miner.json ./out/unitTest.txt ./out/unitEncToM2.txt 2dnzkaaKeeCeUAXTy2DrxijSKGB 3SEdktQGS4K947PUadvbHFD2oJG
 ) else if "%choice%"=="6" (
-    node index.js -tn miner2.json ./out/unitEncToM2.txt 3SEdktQGS4K947PUadvbHFD2oJG
+    node index.js -tn ./examples/miner2.json ./out/unitEncToM2.txt 3SEdktQGS4K947PUadvbHFD2oJG
 ) else if "%choice%"=="7" (
-    node index.js -to miner.json ./out/unitTest.txt 3SEdktQGS4K947PUadvbHFD2oJG
+    node index.js -to ./examples/miner.json ./out/unitTest.txt.0 3SEdktQGS4K947PUadvbHFD2oJG
 ) else if "%choice%"=="8" (
-    node index.js -tn miner2.json ./out/unitTest.txt 3SEdktQGS4K947PUadvbHFD2oJG
+    node index.js -tn ./examples/miner2.json ./out/unitTest.txt.0 3SEdktQGS4K947PUadvbHFD2oJG
 ) else if "%choice%"=="9" (
-    node index.js -cCuda miner.json ./out/unitTest.txt 3SEdktQGS4K947PUadvbHFD2oJG
+    node index.js -cc ./examples/miner.json ./out/unitTest.txt 3SEdktQGS4K947PUadvbHFD2oJG
 ) else (
     echo "Invalid Choice. Try Again" 
 )
@@ -40,7 +40,7 @@ if "%choice%"=="1" (
     goto:choosing
 	
 :diceVal
-node index.js -c miner.json ./out/unitTest.txt 3SEdktQGS4K947PUadvbHFD2oJG %value%
+node index.js -cc ./examples/miner.json ./out/unitTest.txt 3SEdktQGS4K947PUadvbHFD2oJG %value%
 goto:choosing
 
 pause
